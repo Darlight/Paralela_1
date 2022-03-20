347ba6c6856afc6756126c7b3ea6970ec7227272
JDiegoS <46642689+JDiegoS@users.noreply.github.com>
2022-03-19 19:08:32 -0600

    Correcciones

--- Proyecto1.c
+++ Proyecto1.c
@@ -18,10 +18,23 @@ int main(int argc, char* argv[])
 	double L = 100;
 	double t0 = 40;			// temperatura inicial de toda la barra
 	double tL = 40;			// temperatura en la frontera izquierda
-	double tR = 70;			// temperatura en la frontera derecha
+	double tR = 100;			// temperatura en la frontera derecha
 	double dt = 1000000;			
 	double dx = L/ni;			
 	double c = 0.000001;	// constante 10e-5
+
+	printf("Ingrese el numero de itervalos: ");
+	scanf("%d", &ni);
+	printf("Ingrese la longitud: ");
+	scanf("%lf", &L);
+	printf("Ingrese a temperatura inicial de la barra: ");
+	scanf("%lf", &t0);
+	printf("Ingrese la temperatura de la frontera izquierda: ");
+	scanf("%lf", &tL);
+	printf("Ingrese la temperatura de la frontera derecha: ");
+	scanf("%lf", &tR);
+
+	printf("Calculando con %d intervalos discretos\n", ni);
 
 	// Vectores
 	double t[ni];
@@ -38,39 +51,23 @@ int main(int argc, char* argv[])
 
 	error = 1000;
 	int interv = 0;
+	double tError;
 	// while no llegamos a criterio de salida
-	while (interv < ni && error >= 0.0001 )
+	while (interv < 1000 && error >= 0.001 )
 	{
+		tError = 0;
 		// Calcular nueva temperatura
-		// Paralelizado con scheduling o con ompthreads clasico
 		for (int j = 0; j < ni; j++)
 		{
 			next_t[j] = t[j] + ((c*dt)/pow(dx, 2)) * (t[j-1] - 2*t[j] + t[j+1]);
-		}
-
-		// Calcular error
-		// Paralelizado con scheduling o con ompthreads clasico
-		for (int i = 0; i <= ni - 1; i++)
-		{
-			double de = abs(next_t[i] - t[i]);	//Absolute Error
-			if (de > error)
-				error = de;			//Maximum error
-
-		}
-
-		// PENSAR EN COMO DIVIDIR ARREGLOS PARA PARALELIZACION
-
-		if (interv == ni - 1){
-			printf("Iteracion %d \n", interv);
-			for (int j = 0; j < ni; j++) {
-				printf("%f ", t[j]);
+			
+			// Calcular error
+			double newError = fabs(next_t[j] - t[j]);
+			if (newError > tError){
+				tError = newError;
 			}
-			printf("\n\n Vector solucion \n");
-			for (int j = 0; j < ni; j++) {
-				printf("%f ", next_t[j]);
-			}
-			printf("\n\n");
 		}
+		error = tError;
 
 		// Actualizar vectores
 		for (int j = 0; j < ni; j++) {
@@ -79,6 +76,11 @@ int main(int argc, char* argv[])
 			}
 		interv++;
 	}
+	printf("\nIteracion %d\n\n Vector solucion \n", interv);
+	for (int j = 0; j < ni; j++) {
+		printf("%f ", t[j]);
+	}
+	printf("\n\nError = %lf\n", error);
 
 	
 }
